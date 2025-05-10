a980b25df57ecf0fbe7da540bf8cc1ab756a78a1
Steven-Goose <scole2@horizon.csueastbay.edu>
2025-05-10 00:43:24 -0700

    Properly uploading the cloned repo...?

---
+++ computational-physics-python/jupyter_notebook_config.py
@@ -0,0 +1,5 @@
+import os
+c = get_config()
+c.NbConvertApp.export_format = 'pdf'
+c.TemplateExporter.template_path = ['.', os.path.expanduser('~/.jupyter/templates/')]
+c.Exporter.template_file = 'mytemplate'
